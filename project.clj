320bf1de8346d12870c93ac471da9ff885456d8f
Paul Williams <paul@paulswilliams.me.uk>
2016-07-14 07:32:46 +0100

    added cucumber dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.12.0"]
-                 [org.clojars.pallix/analemma "1.0.0"]])
+                 [clj-time "0.12.0"]]
+  :plugins [[lein-cucumber "1.0.2"]])
