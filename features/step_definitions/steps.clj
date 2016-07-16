9e5e04a8096b19a71c7c50747369b709524e9b79
Paul Williams <paul@paulswilliams.me.uk>
2016-07-16 17:30:58 +0100

    graph feature given step invoking repo with list of Fillups

--- features/step_definitions/steps.clj
+++ features/step_definitions/steps.clj
@@ -1,7 +1,21 @@
+(require '[lease-car-finances.repository :as repository]
+         '[lease-car-finances.core :as core]
+         '[clj-time.core :as t]
+         '[clj-time.format :as f])
+
+(use 'clojure.walk)
+
 (Given #"^the following recorded fill ups$" [fill-ups]
-       (comment  Express the Regexp above with the code you wish you had  )
+       (->>
+         fill-ups
+         .asMaps
+         (map #(-> (into {} %)
+                   keywordize-keys
+                   (update :date (fn [d] (f/parse (f/formatter :date) d)))
+                   core/map->Fillup))
+         repository/create)
        (throw (cucumber.runtime.PendingException.)))
 
 (Then #"^I will see a graph presenting$" []
-      (comment  Express the Regexp above with the code you wish you had  )
+      (comment Express the Regexp above with the code you wish you had)
       (throw (cucumber.runtime.PendingException.)))
---
+++ src/lease_car_finances/repository.clj
@@ -0,0 +1,6 @@
+(ns lease-car-finances.repository)
+
+(defn clear-down [])
+
+(defn create [fill-ups]
+  (println fill-ups))
