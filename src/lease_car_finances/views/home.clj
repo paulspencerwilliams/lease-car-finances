2095d3c257ff0e7db90478aa7f607f211de5c885
Paul Williams <paul@paulswilliams.me.uk>
2016-07-31 19:45:40 +0100

    Refactored script tags into home template

--- src/lease_car_finances/views/home.clj
+++ src/lease_car_finances/views/home.clj
@@ -3,4 +3,10 @@
             [lease-car-finances.views.layout :as layout]))
 
 (defn view [mpgs]
-  (layout/common [:div#chartContainer] mpgs))
+  (layout/common
+    [:html
+     [:script (str "var mpgs = " mpgs ";")]
+     (include-js "https://code.jquery.com/jquery-3.1.0.min.js")
+     (include-js "https://code.highcharts.com/highcharts.js")
+     (include-js "js/mpg-graph.js")]
+    [:div#chartContainer]))
--- src/lease_car_finances/views/layout.clj
+++ src/lease_car_finances/views/layout.clj
@@ -1,12 +1,9 @@
 (ns lease-car-finances.views.layout
   (:require [hiccup.page :refer [html5 include-js]]))
 
-(defn common [body-content mpgs]
+(defn common [scripts body-content]
   (html5
     [:head
      [:title "Welcome to lease car finances"]
-     (include-js "https://code.jquery.com/jquery-3.1.0.min.js")
-     (include-js "https://code.highcharts.com/highcharts.js")
-     [:script (str "var mpgs = " mpgs ";")]
-     (include-js "js/mpg-graph.js")]
+     scripts]
     [:body body-content]))
